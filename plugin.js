5e1c3854be2a9f4a38e3a8d7c0084a3c5db2baff
sadip14 <satryaadianpratama@gmail.com>
2026-06-13 00:12:29 +0700

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,16 +1,16 @@
-const config = {
-    name: "Otakudesu Stream",
-    id: "otakudesu-satrya",
-    baseUrl: "https://otakudesu.blog"
-};
+const PLUGIN_ID = "otakudesu-satrya";
+const BASE_URL = "https://otakudesu.blog";
 
-// fix error: pake http client bawaan grayjay
-const client = http; 
+// 1. fungsi wajib biar plugin ga error pas dihidupin/dimatiin
+source.enable = function(conf) {};
+source.disable = function() {};
 
-function getHome() {
-    // diganti pake client.GET biar ga reference error
-    const response = client.GET(config.baseUrl, {});
-    const doc = dom.parse(response);
+// 2. nampilin home (ongoing anime)
+source.getHome = function() {
+    const resp = http.GET(BASE_URL, {});
+    // handle respons bentuk objek atau string
+    const html = typeof resp === 'string' ? resp : resp.body;
+    const doc = dom.parse(html);
     
     const kotakAnime = doc.select("div.detpost"); 
     const results = [];
@@ -21,37 +21,61 @@ function getHome() {
         const gambar = kotak.select("img").attr("src");
         const linkNonton = kotak.select("a").attr("href");
         
+        // format udah full ngikutin PlatformVideoDef lu
         results.push(new PlatformVideo({
-            id: linkNonton,
+            id: new PlatformID("otakudesu", linkNonton, PLUGIN_ID),
             name: judul,
-            thumbnails: [new Thumbnail(gambar)],
-            author: new PlatformAuthorLink(new PlatformID(config.id, "otakudesu", config.id), "Otakudesu", config.baseUrl)
+            thumbnails: new Thumbnails([new Thumbnail(gambar, 0)]),
+            author: new PlatformAuthorLink(new PlatformID("otakudesu", "author", PLUGIN_ID), "Otakudesu", BASE_URL, "", 0),
+            uploadDate: Math.floor(Date.now() / 1000), // asal isi biar grayjay ga marah
+            url: linkNonton,
+            duration: 1440,
+            viewCount: 0,
+            isLive: false
         }));
     }
     
     return new VideoPager(results, false);
-}
+};
 
-function search(query) {
-    const searchUrl = `${config.baseUrl}/?s=${encodeURIComponent(query)}&submit=Search`;
-    const response = client.GET(searchUrl, {});
-    const doc = dom.parse(response);
+// 3. nampilin hasil search
+source.search = function(query, type, order, filters) {
+    const searchUrl = BASE_URL + "/?s=" + encodeURIComponent(query) + "&submit=Search";
+    const resp = http.GET(searchUrl, {});
+    const html = typeof resp === 'string' ? resp : resp.body;
+    const doc = dom.parse(html);
     
     const listSearch = doc.select(".chivsrc li");
     const results = [];
     
     for (let i = 0; i < listSearch.length; i++) {
         const kotak = listSearch[i];
+        const linkNonton = kotak.select("a").attr("href");
+        const judul = kotak.select("h2").text();
+        const gambar = kotak.select("img").attr("src");
+        
         results.push(new PlatformVideo({
-            id: kotak.select("a").attr("href"),
-            name: kotak.select("h2").text(),
-            thumbnails: [new Thumbnail(kotak.select("img").attr("src"))],
-            author: new PlatformAuthorLink(new PlatformID(config.id, "otakudesu", config.id), "Otakudesu", config.baseUrl)
+            id: new PlatformID("otakudesu", linkNonton, PLUGIN_ID),
+            name: judul,
+            thumbnails: new Thumbnails([new Thumbnail(gambar, 0)]),
+            author: new PlatformAuthorLink(new PlatformID("otakudesu", "author", PLUGIN_ID), "Otakudesu", BASE_URL, "", 0),
+            uploadDate: Math.floor(Date.now() / 1000),
+            url: linkNonton,
+            duration: 1440,
+            viewCount: 0,
+            isLive: false
         }));
     }
     
     return new VideoPager(results, false);
-}
+};
 
-source.getHome = getHome;
-source.search = search;
+// 4. perintilan wajib lainnya biar ga error reference
+source.getSearchCapabilities = function() { return new ResultCapabilities([], [], []); };
+source.searchSuggestions = function(query) { return []; };
+source.isChannelUrl = function(url) { return false; };
+source.getChannel = function(url) { throw new ScriptException("Not implemented"); };
+source.getChannelVideos = function(url, type, order, filters) { return new VideoPager([], false); };
+source.getChannelCapabilities = function() { return new ResultCapabilities([], [], []); };
+source.isVideoDetailsUrl = function(url) { return url.includes("otakudesu.blog/anime/"); };
+source.getVideoDetails = function(url) { throw new ScriptException("Belum dibikin wkwk"); };
