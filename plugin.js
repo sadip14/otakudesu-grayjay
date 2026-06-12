14d7322d55135a3191459658c18bd417c1ac7557
sadip14 <satryaadianpratama@gmail.com>
2026-06-13 00:02:53 +0700

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,16 +1,17 @@
-// nyiapin konfigurasi source utama grayjay
 const config = {
     name: "Otakudesu Stream",
     id: "otakudesu-satrya",
     baseUrl: "https://otakudesu.blog"
 };
 
-// ini fungsi wajib biar grayjay tau cara nampilin halaman utama
+// fix error: pake http client bawaan grayjay
+const client = http; 
+
 function getHome() {
-    const response = http.GET(config.baseUrl, {});
+    // diganti pake client.GET biar ga reference error
+    const response = client.GET(config.baseUrl, {});
     const doc = dom.parse(response);
     
-    // selector hasil buruan lu kemarin
     const kotakAnime = doc.select("div.detpost"); 
     const results = [];
     
@@ -20,7 +21,6 @@ function getHome() {
         const gambar = kotak.select("img").attr("src");
         const linkNonton = kotak.select("a").attr("href");
         
-        // format wajib penulisan object video di grayjay
         results.push(new PlatformVideo({
             id: linkNonton,
             name: judul,
@@ -32,10 +32,9 @@ function getHome() {
     return new VideoPager(results, false);
 }
 
-// ini fungsi wajib biar grayjay bisa pake kolom search
 function search(query) {
     const searchUrl = `${config.baseUrl}/?s=${encodeURIComponent(query)}&submit=Search`;
-    const response = http.GET(searchUrl, {});
+    const response = client.GET(searchUrl, {});
     const doc = dom.parse(response);
     
     const listSearch = doc.select(".chivsrc li");
@@ -54,6 +53,5 @@ function search(query) {
     return new VideoPager(results, false);
 }
 
-// ngedaftarin semua fungsi ke sistem grayjay
 source.getHome = getHome;
 source.search = search;
